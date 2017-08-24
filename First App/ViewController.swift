6bb0f8da56695a01e3a129e50126188fa04ff90a
sahand sadri <sahandsadri@mwc-005158.dhcp.missouri.edu>
2017-08-24 12:51:54 -0500

    Initial Commit

---
+++ First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First App
+//
+//  Created by sahand sadri on 8/24/17.
+//  Copyright © 2017 Sahand Sadri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
